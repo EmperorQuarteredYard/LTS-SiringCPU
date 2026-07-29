954d58ec32d35251bdc06102dfcd34a5ffa8f5c9
EmperorQuarteredYard <guanba.genshin@gmail.com>
2026-07-29 21:18:52 +0800

    perf(IDU)更改了IDU在FPGA下的实现

--- src/IDU.v
+++ src/IDU.v
@@ -14,6 +14,12 @@ module IDU
 	output [ 4:0] IDU_GPR_addr2,
 	input  [31:0] IDU_GPR_data1,
 	input  [31:0] IDU_GPR_data2,//IDU通过ISU访问GPR；GPR的读行为不需要经过一拍
+    // output [ 4:0] IDU_GPR_rkaddr,
+    // output [ 4:0] IDU_GPR_rjaddr,
+    // output [ 4:0] IDU_GPR_rdaddr,
+    // input  [31:0] GPR_IDU_rkdata,
+    // input  [31:0] GPR_IDU_rjdata,
+    // input  [31:0] GPR_IDU_rddata,
 
 	output [31:0] IDU_ISU_PCnew,
 	output        IDU_ISU_PCmis,
@@ -27,6 +33,10 @@ module IDU
 	output [10:0] IDU_EXU_func,//这里描述得到的结果是什么含义，0-0-0-0-0-0-0-0-st-ld-alu运算
 	input  [31:0] EXU_IDU_ready
 );
+`include "define.vh"
+`define OP_ANALYSE_FPGA
+`endif
+
 reg [31:0] reg_PC;
 reg [1:0]  reg_PCid;
 reg        reg_valid;
@@ -49,7 +59,7 @@ assign ISU_IDU_handshake = IDU_ISU_valid & ISU_IDU_ready;//其实这里并没有
 assign IDU_EXU_handshake = IDU_EXU_valid & EXU_IDU_ready;
 `endif
 
-`define OP_ANALYSE//指令解析
+`define OP_ANALYSE
 `ifdef OP_ANALYSE
 
 assign wire_PC = reg_PC;
@@ -67,17 +77,8 @@ wire [19:0] i20;
 wire [15:0] i16;
 wire [25:0] i26;
 
-wire [63:0] op_31_26_d;
-wire [15:0] op_25_22_d;
-wire [ 3:0] op_21_20_d;
-wire [31:0] op_19_15_d;
-
 assign {op_31_26,op_25_22,op_21_20,op_19_15,rk,rj,rd} = wire_inst;
 
-decoder_6_64 u_dec0(.in(op_31_26 ), .out(op_31_26_d ));
-decoder_4_16 u_dec1(.in(op_25_22 ), .out(op_25_22_d ));
-decoder_2_4  u_dec2(.in(op_21_20 ), .out(op_21_20_d ));
-decoder_5_32 u_dec3(.in(op_19_15 ), .out(op_19_15_d ));
 wire        inst_ld_o_st;
 wire        inst_ld_x;
 wire        inst_st_x;
@@ -111,6 +112,17 @@ wire        inst_csrwr;
 wire        inst_csrxchg;
 wire        inst_cacop;
 
+`ifdef ENVIRONMENT_ASIC
+wire [63:0] op_31_26_d;
+wire [15:0] op_25_22_d;
+wire [ 3:0] op_21_20_d;
+wire [31:0] op_19_15_d;
+
+decoder_6_64 u_dec0(.in(op_31_26 ), .out(op_31_26_d ));
+decoder_4_16 u_dec1(.in(op_25_22 ), .out(op_25_22_d ));
+decoder_2_4  u_dec2(.in(op_21_20 ), .out(op_21_20_d ));
+decoder_5_32 u_dec3(.in(op_19_15 ), .out(op_19_15_d ));
+
 assign inst_ld_o_st      = op_31_26_d[6'h0a] & ~op_25_22[3];
 assign inst_ld_x         = inst_ld_o_st & ~op_25_22[2];
 assign inst_st_x         = inst_ld_o_st &  op_25_22[2];
@@ -143,6 +155,43 @@ assign inst_cpucfg    = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h0] 
 assign inst_csrwr     = op_31_26_d[6'h01] & ~op_25_22[3]     & ~op_25_22[2] & (rj == 5'b00011);
 assign inst_csrxchg   = op_31_26_d[6'h01] & ~op_25_22[3]     & ~op_25_22[2] & (rj != 5'b00001) & (rj != 5'b0);
 assign inst_cacop     = op_31_26_d[6'h01] & op_25_22_d[4'h8];
+`endif
+
+`ifdef ENVIRONMENT_FPGA
+
+assign inst_ld_o_st      = (op_31_26 == 6'h0a) & ~op_25_22[3];
+assign inst_ld_x         = inst_ld_o_st & ~op_25_22[2];
+assign inst_st_x         = inst_ld_o_st &  op_25_22[2];
+
+assign inst_lu12i_w   = (op_31_26 == 6'h05) & ~op_25_22[3];
+assign inst_pcaddu12i = (op_31_26 == 6'h07) & ~op_25_22[3];
+assign inst_addi_w    = (op_31_26 == 6'h00) & (op_25_22 == 4'ha);
+assign inst_add_w     = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1);
+assign inst_sub_w     = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h02);
+assign inst_slt       = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h04);
+assign inst_and       = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h09);
+assign inst_andi      = (op_31_26 == 6'h00) & (op_25_22 == 4'hd);
+assign inst_or        = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h0a);
+assign inst_ori       = (op_31_26 == 6'h00) & (op_25_22 == 4'he);
+assign inst_xor       = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h0b);
+assign inst_sll_w     = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h0e);
+assign inst_slli_w    = (op_31_26 == 6'h00) & (op_25_22 == 4'h1) & (op_21_20 == 2'h0) & (op_19_15 == 5'h01);
+assign inst_srli_w    = (op_31_26 == 6'h00) & (op_25_22 == 4'h1) & (op_21_20 == 2'h0) & (op_19_15 == 5'h09);
+assign inst_ld_b      = (op_31_26 == 6'h0a) & (op_25_22 == 4'h0);
+assign inst_ld_w      = (op_31_26 == 6'h0a) & (op_25_22 == 4'h2);
+assign inst_st_b      = (op_31_26 == 6'h0a) & (op_25_22 == 4'h4);
+assign inst_st_w      = (op_31_26 == 6'h0a) & (op_25_22 == 4'h6);
+assign inst_b         = (op_31_26 == 6'h14);
+assign inst_bl        = (op_31_26 == 6'h15);
+assign inst_beq       = (op_31_26 == 6'h16);
+assign inst_bne       = (op_31_26 == 6'h17);
+assign inst_jirl      = (op_31_26 == 6'h13);
+assign inst_mul_w     = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h1) & (op_19_15 == 5'h18);
+assign inst_cpucfg    = (op_31_26 == 6'h00) & (op_25_22 == 4'h0) & (op_21_20 == 2'h0) & (op_19_15 == 5'h00) & (rk ==  5'b11011);
+assign inst_csrwr     = (op_31_26 == 6'h01) & ~op_25_22[3]     & ~op_25_22[2] & (rj ==  5'b00011);
+assign inst_csrxchg   = (op_31_26 == 6'h01) & ~op_25_22[3]     & ~op_25_22[2] & (rj != 5'b00001) & (rj != 5'b0);
+assign inst_cacop     = (op_31_26 == 6'h01) & (op_25_22 == 4'h8); 
+`endif
 `endif
 
 `define IMM_ANALYSE//立即数分析
